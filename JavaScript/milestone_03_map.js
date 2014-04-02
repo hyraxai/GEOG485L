cd8798344fcc76933e17bcb290947e59b2c90906
lpa2 <lpa2gis@gmail.com>
2014-04-02 17:19:52 -0600

    no message

--- JavaScript/milestone_03_map.js
+++ JavaScript/milestone_03_map.js
@@ -6,7 +6,9 @@ function initialize(){
         mapTypeId: google.maps.MapTypeId.ROAD};
     var map = new google.maps.Map(document.getElementById("milestone_03_map"), mapOptions);
     var WiFiLayer = new google.maps.KmlLayer({
-        url: 'http://lpa2.github.io/GEOG485L/XML/WigleWifi_20140207085042.kml'
+        url: 'http://lpa2.github.io/GEOG485L/XML/milestone_03.kml'
     });
-        WiFiLayer.setMap(map);
+        WiFiLayer.setMap(milestone_03_map);
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
