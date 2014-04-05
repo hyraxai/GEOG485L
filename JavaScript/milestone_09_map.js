a746f1cd0afb8284ec71cff385336d23408d041b
lpa2 <lpa2gis@gmail.com>
2014-04-05 10:53:57 -0600

    no message

--- JavaScript/milestone_09_map.js
+++ JavaScript/milestone_09_map.js
@@ -32,37 +32,136 @@ function initialize(){
     Layers.BeachParks = new OpenLayers.Layer.Vector('Beach Parks');
     map.addLayer(Layers.BeachParks);
 
-    myOSMPointStyle = {
+    BeachPark = {
       pointRadius: 12,
-      fillColor: "CC0000",
+      fillColor: "0000FF",
       fillOpacity: 1,
       strokeOpacity: 1
     }
 
     var Coord_EastBeachPark = new OpenLayers.Geometry.Point(-119.661723, 34.419577).transform(
-					new OpenLayers.Projection("EPSG:4326"),
-				  map.getProjectionObject()
-					);
-    var Point_EastBeachPark = new OpenLayers.Feature.Vector(Coord_EastBeachPark,myOSMPointStyle);
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				);
+    var Point_EastBeachPark = new OpenLayers.Feature.Vector(Coord_EastBeachPark,BeachPark);
     var Coord_LeadbetterBeachPark = new OpenLayers.Geometry.Point(-119.698047, 34.402341).transform(
-					new OpenLayers.Projection("EPSG:4326"),
-				  map.getProjectionObject()
-					);
-    var Point_LeadbetterBeachPark = new OpenLayers.Feature.Vector(Coord_LeadbetterBeachPark,myOSMPointStyle);
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				);
+    var Point_LeadbetterBeachPark = new OpenLayers.Feature.Vector(Coord_LeadbetterBeachPark,BeachPark);
     var Coord_MesaLaneSteps = new OpenLayers.Geometry.Point(-119.730702, 34.398399).transform(
-					new OpenLayers.Projection("EPSG:4326"),
-				  map.getProjectionObject()
-					);
-    var Point_MesaLaneSteps = new  OpenLayers.Feature.Vector(Coord_MesaLaneSteps,myOSMPointStyle);
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				);
+    var Point_MesaLaneSteps = new  OpenLayers.Feature.Vector(Coord_MesaLaneSteps,BeachPark);
     var Coord_ThousandSteps = new OpenLayers.Geometry.Point(-119.713646, 34.396243).transform(
-					new OpenLayers.Projection("EPSG:4326"),
-				  map.getProjectionObject()
-					);
-    var Point_ThousandSteps = new  OpenLayers.Feature.Vector(Coord_ThousandSteps,myOSMPointStyle);
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				);
+    var Point_ThousandSteps = new  OpenLayers.Feature.Vector(Coord_ThousandSteps,BeachPark);
     var Coord_WestBeachPark = new OpenLayers.Geometry.Point(-119.690929, 34.410744).transform(
-					new OpenLayers.Projection("EPSG:4326"),
-				  map.getProjectionObject()
-					);
-    var Point_WestBeachPark = new  OpenLayers.Feature.Vector(Coord_WestBeachPark,myOSMPointStyle);
-        Layers ["BeachParks"].addFeatures([Point_EastBeachPark, Point_LeadbetterBeachPark, Point_MesaLaneSteps, Point_ThousandSteps, Point_WestBeachPark]);
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				);
+    var Point_WestBeachPark = new  OpenLayers.Feature.Vector(Coord_WestBeachPark,BeachPark);
+
+    Layers ["BeachParks"].addFeatures([Point_EastBeachPark, Point_LeadbetterBeachPark, Point_MesaLaneSteps, Point_ThousandSteps, Point_WestBeachPark]);
+
+    Layers.Harbor = new OpenLayers.Layer.Vector('Santa Barbara Harbor');
+    map.addLayer(Layers.Harbor);
+
+ 		HarborOutline = {
+		  fillColor: "#88ff00",
+		  fillOpacity: .2,
+		  strokeOpacity: .5,
+		  strokeColor: "#88ff00"
+    }
+
+    var Coord_SantaBarbaraHarbor = new OpenLayers.Geometry.LinearRing([
+    		new OpenLayers.Geometry.Point(-119.694208, 34.404775).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+			  map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.692133, 34.403253).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+			  map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.691850, 34.403156).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+			  map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.687556, 34.404958).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.685103, 34.408036).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.685964, 34.409789).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.688297, 34.411161).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.690569, 34.408494).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.690831, 34.406414).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.691075, 34.407100).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.691261, 34.408094).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+        new OpenLayers.Geometry.Point(-119.691881, 34.408478).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.693339, 34.405983).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.693742, 34.405511).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    		new OpenLayers.Geometry.Point(-119.694208, 34.404775).transform(
+				new OpenLayers.Projection("EPSG:4326"),
+				map.getProjectionObject()
+				),
+    ]);
+    var LinearRing_SantaBarbaraHarbor = new OpenLayers.Feature.Vector(Coord_SantaBarbaraHarbor, null, HarborOutline);
+
+    Layers["Harbor"].addFeatures([LinearRing_SantaBarbaraHarbor]);
+
+    
+    myKMLstyle = {
+    			fillColor: "#0000ff",
+    			fillOpacity: 1,
+    			strokeOpacity: 1,
+    			strokeColor: "#0000ff"}
+
+
+    Layers.SantaBarbaraChannel = new OpenLayers.Layer.Vector("Santa Barbara Channel", {
+                projection: map.displayProjection,
+                visibility: false,
+                style: myKMLstyle,
+                strategies: [new OpenLayers.Strategy.Fixed()],
+                protocol: new OpenLayers.Protocol.HTTP({
+                    url: "http://lpa2.github.io/GEOG485L/XML/milestone_09.kml",
+                    format: new OpenLayers.Format.KML({
+                        extractAttributes: true
+                    })
+                })
+            });
+           	map.addLayer(Layers.states)
 }
