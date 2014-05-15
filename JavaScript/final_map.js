508c2bc0af12212e4b3bf495273e1611473f3946
lpa2 <lpa2gis@gmail.com>
2014-05-15 09:30:43 -0600

    bug fixes.

--- JavaScript/final_map.js
+++ JavaScript/final_map.js
@@ -1,14 +1,13 @@
 function initialize(){
-    var lon = -106.5;
-    var lat = 36;
-    var zoom = 3;
+    var lon = -106.567711;
+    var lat = 35.244414;
+    var zoom = 10;
     var myProjection = new OpenLayers.Projection("EPSG:4326")
-    var bmngLayer, countiesLayer, indlandLayer, federallandsLayer;
-    var map = new OpenLayers.Map('milestone_13_map',
+    var map = new OpenLayers.Map('final_map',
             	{projection:myProjection}
     );
 
-		usImagery = new OpenLayers.Layer.WMS( 
+		usImagery = new OpenLayers.Layer.WMS(
       "US Imagery Tile Service - USGS", 
       "http://basemap.nationalmap.gov/arcgis/services/USGSImageryTopo/MapServer/WmsServer?",
       {layers: "0", version: '1.3.0', transparent: 'TRUE'},
@@ -16,38 +15,55 @@ function initialize(){
     );
     map.addLayer(usImagery)
 
-    nmeocLayer = new OpenLayers.Layer.WMS( 
-      "New Mexico Emergency Operation Centers", 
+    bernLayer = new OpenLayers.Layer.WMS(
+      "tgr2006se_bern_lka",
       "http://geog485.unm.edu:8080/geoserver/s_alder117/ows?",
-      {layers: "2007_07_26_nm_eoc", styles: 'EOC_Diamond', version: '1.1.1', transparent: 'true'},
+      {layers: "tgr2006se_bern_lka", version: '1.1.1', transparent: 'true'},
       {isBaseLayer: false, visibility: true}
     );
-    map.addLayer(nmeocLayer)
+    map.addLayer(bernLayer)
 
-	  nmfirestations = new OpenLayers.Layer.WMS( 
-      "New Mexico Firestations", 
+
+    a5contLayer = new OpenLayers.Layer.WMS(
+      "35106-A5_CONT",
       "http://geog485.unm.edu:8080/geoserver/s_alder117/ows?",
-      {layers: "2008_09_19_nm_firestations", styles: 'FireStation_Triangle', version: '1.1.1', transparent: 'true'},
+      {layers: "35106-A5_CONT", version: '1.1.1', transparent: 'true'},
       {isBaseLayer: false, visibility: true}
     );
-    map.addLayer(nmfirestations)
+    map.addLayer(a5contLayer)
+
+	  a6contLayer = new OpenLayers.Layer.WMS(
+      "35106-A6_CONT",
+      "http://geog485.unm.edu:8080/geoserver/s_alder117/ows?",
+      {layers: "35106-A6_CONT", version: '1.1.1', transparent: 'true'},
+      {isBaseLayer: false, visibility: true}
+    );
+    map.addLayer(a6contLayer)
 		
-    nmhospitals = new OpenLayers.Layer.WMS( 
-      "New Mexico Hospitals", 
+    a5tifLayer = new OpenLayers.Layer.WMS(
+      "35106-A5",
       "http://geog485.unm.edu:8080/geoserver/s_alder117/ows?",
-      {layers: "2008_09_20_nm_hospitals", styles: 'Hospital_Polygon', version: '1.1.1', transparent: 'true'},
+      {layers: "35106-A5", styles: 'dem', version: '1.1.1', transparent: 'true'},
       {isBaseLayer: false, visibility: true}
     );
-    map.addLayer(nmhospitals)
+    map.addLayer(a5tifLayer)
 
-    countiesLayer = new OpenLayers.Layer.WMS( 
+    a6tifLayer = new OpenLayers.Layer.WMS(
+      "35106-A5",
+      "http://geog485.unm.edu:8080/geoserver/s_alder117/ows?",
+      {layers: "35106-A6", styles: 'dem', version: '1.1.1', transparent: 'true'},
+      {isBaseLayer: false, visibility: true}
+    );
+    map.addLayer(a6tifLayer)
+
+    uscountiesLayer = new OpenLayers.Layer.WMS(
       "US Counties", 
       "http://webservices.nationalatlas.gov/wms?",
       {layers: "counties", version: '1.3.0', transparent: 'TRUE'},
-      {isBaseLayer: false, visibility: false, opacity: .8}
+      {isBaseLayer: false, visibility: true, opacity: .8}
     );
 
-    map.addLayer(countiesLayer);
+    map.addLayer(uscountiesLayer);
 									
 		map.setCenter(
         new OpenLayers.LonLat(lon, lat), zoom
